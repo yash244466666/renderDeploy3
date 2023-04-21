e9c82cb5e78642a30a1b9ef550852efecff292da
Yash <74345713+yash244466666@users.noreply.github.com>
2023-04-21 14:24:44 +0600

    Add 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get "about" => 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
